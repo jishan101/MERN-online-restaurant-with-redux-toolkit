8cd5b37001f832bc2d0d550067ff57df4f0d9afc
jishan101 <rko.jishan@gmail.com>
2022-06-17 03:08:27 +0600

    added shipping address and phone in stripe checkout

--- server-side/routes/stripeRoutes.js
+++ server-side/routes/stripeRoutes.js
@@ -33,9 +33,9 @@ FoodItem.find().sort({ createdAt: -1 })
 router.post('/create-checkout-session', async (req, res) => {
     let cartItems = [];
 
-    for(const cartItem of req.body.cartItems) {
-        for(const foodItem of foodItems) {
-            if(cartItem._id === foodItem._id.toString()) {
+    for (const cartItem of req.body.cartItems) {
+        for (const foodItem of foodItems) {
+            if (cartItem._id === foodItem._id.toString()) {
                 cartItems.push({
                     ...foodItem,
                     quantity: cartItem.quantity
@@ -57,10 +57,43 @@ router.post('/create-checkout-session', async (req, res) => {
                 },
                 unit_amount: cartItem.price * 100,
             },
+            adjustable_quantity: {
+                enabled: true,
+                minimum: 1,
+            },
             quantity: cartItem.quantity,
         }
     })
     const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        shipping_address_collection: {
+            allowed_countries: ['US', 'CA', 'BD'],
+        },
+        shipping_options: [
+            {
+                shipping_rate_data: {
+                    type: 'fixed_amount',
+                    fixed_amount: {
+                        amount: 0,
+                        currency: 'usd',
+                    },
+                    display_name: 'Free Shipping',
+                    delivery_estimate: {
+                        minimum: {
+                            unit: 'hour',
+                            value: 1,
+                        },
+                        maximum: {
+                            unit: 'hour',
+                            value: 1,
+                        },
+                    }
+                }
+            }
+        ],
+        phone_number_collection: {
+            enabled: true,
+        },
         line_items,
         mode: 'payment',
         success_url: `${process.env.GOODFOOD_FRONTEND_URL}/payment-success`,
