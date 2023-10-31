d7e13afead64e42a4b7051942d8d21634c5437c6
Irfanul Islam <irfanul.islam@bengalmobileqa.com>
2023-11-01 02:16:03 +0600

    fix(): checkout total 0.00 issue

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,32 +1,31 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import NotFound from "./NotFound";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Home from './pages/home/Home';
-import FoodItemsView from './features/fooditems/FoodItemsView';
-import CartView from './features/cart/cartView';
-import PaymentSuccess from './pages/paymentSuccess/PaymentSuccess';
-import NotFound from './NotFound';
-import Footer from './components/Footer';
-import Contacts from './pages/contacts/Contacts';
-import About from './pages/about/About';
+import CartView from "./features/cart/cartView";
+import FoodItemsView from "./features/fooditems/FoodItemsView";
+import About from "./pages/about/About";
+import Contacts from "./pages/contacts/Contacts";
+import Home from "./pages/home/Home";
+import PaymentSuccess from "./pages/paymentSuccess/PaymentSuccess";
 
 function App() {
-    return (
-        <div className="App text-secondary-color font-league-spartan leading-relaxed">
-            <Header />
-            <Routes>
-                <Route path='/' element={ <Home /> } />
-                <Route path='/menu' element={ <FoodItemsView /> } />
-                <Route path='/about' element={ <About /> } />
-                <Route path='/contacts' element={ <Contacts /> } />
-                <Route path='/cart' element={ <CartView /> } />
-                <Route path='/payment-success' element={ <PaymentSuccess /> } />
-                <Route path='*' element={ <NotFound /> } />
-            </Routes>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="App text-secondary-color font-league-spartan leading-relaxed">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<FoodItemsView />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/cart" element={<CartView />} />
+        <Route path="/payment-success" element={<PaymentSuccess />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
-
