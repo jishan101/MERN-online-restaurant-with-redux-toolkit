7c3c4befe6300d21423fcd6ad9768653f837525f
Irfanul Islam <irfanul.islam@bengalmobileqa.com>
2023-11-12 04:42:19 +0600

    chore(): app.listen in the end

--- server-side/app.js
+++ server-side/app.js
@@ -13,14 +13,14 @@ const PORT = process.env.PORT || 8000;
 dotenv.config();
 const dbURI = process.env.MONGODB_URI;
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(result => {
-        app.listen(PORT);
-        console.log("Database connected");
-    })
-    .catch(err => {
-        console.log(err);
-    });
+// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(result => {
+//         app.listen(PORT);
+//         console.log("Database connected");
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -29,25 +29,34 @@ app.set("view engine", "ejs");
 app.use(cors());
 
 app.use(express.static("public"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/fooditems", foodItemRoutes);
 app.use("/stripe", stripeRoutes);
 
 app.get("/", (req, res) => {
-    res.redirect("/new");
+  res.redirect("/new");
 });
 
 app.get("/new", (req, res) => {
-    res.render("createFoodItem", {title: "Create A New Food Item"});
+  res.render("createFoodItem", { title: "Create A New Food Item" });
 });
 
 app.get("/about", (req, res) => {
-    res.render("about", {title: "About Us!"});
+  res.render("about", { title: "About Us!" });
 });
 
 app.use((req, res) => {
-    res.status(404).render("404", {title: "404 Not Found!"});
+  res.status(404).render("404", { title: "404 Not Found!" });
 });
 
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    app.listen(PORT);
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
